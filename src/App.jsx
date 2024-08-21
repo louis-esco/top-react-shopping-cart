12c25246a62a62971cf4b054276359c196d56547
Louis Escoffier <louis.escoffier.bagrel@gmail.com>
2024-08-21 15:47:22 +0200

    Add cart state and function to manage it

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
+  window.cart = cart;
+
+  const getCart = () => {
+    return cart;
+  };
+
+  const getCartLength = () => {
+    return cart.reduce((acc, curr) => acc + curr.qty, 0);
+  };
+
+  const addToCart = (item, qty) => {
+    const newCart = [...cart];
+    const index = newCart.findIndex((el) => el.id === item.id);
+    if (index === -1) newCart.push({ ...item, qty: qty });
+    else newCart[index].qty += qty;
+    setCart(newCart);
+  };
+
   return (
     <>
-      <Navbar />
-      <Outlet />
+      <Navbar getCartLength={getCartLength} />
+      <Outlet context={{ getCart, getCartLength, addToCart }} />
     </>
   );
 }
