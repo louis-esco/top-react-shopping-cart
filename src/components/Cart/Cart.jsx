b55c63c1118d9cab6887c6e5bf7fd6ce5aa7c3dd
Louis Escoffier <louis.escoffier.bagrel@gmail.com>
2024-08-21 16:13:13 +0200

    Display basic cart and allow to remove items

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,3 +1,37 @@
+import { useOutletContext } from "react-router-dom";
+import styles from "./Cart.module.css";
+
 export default function Cart() {
-  return <div>This is cart</div>;
+  const { getCart, getCartPrice, removeFromCart } = useOutletContext();
+  const cart = getCart();
+  const cartPrice = getCartPrice().toFixed(2);
+  return (
+    <>
+      <div className="cartList">
+        {cart.map((item) => (
+          <div key={item.id} className={styles.item}>
+            <div className={styles.itemImg}>
+              <img src={item.image} alt="" />
+            </div>
+            <div className="itemContent">
+              <div className="itemTitle">{item.title}</div>
+              <div className="itemPrice">Unit price: {item.price}€</div>
+              <div className="itemQty">Quantity: {item.qty}</div>
+              <div className="itemTotal">
+                Total price: {item.qty * item.price}€
+              </div>
+              <button
+                onClick={() => {
+                  removeFromCart(item, 1);
+                }}
+              >
+                Remove from cart
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="cartTotal">Cart total price: {cartPrice}€</div>
+    </>
+  );
 }
