950ff4da90e9062f624b336bec9686ff83767d29
Louis Escoffier <louis.escoffier.bagrel@gmail.com>
2024-08-26 17:59:00 +0200

    Add Home content and styles

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,3 +1,15 @@
+import styles from "./Home.module.css";
+import hand from "../../assets/hand.svg";
+
 export default function Home() {
-  return <div>This is home</div>;
+  return (
+    <div className={styles.homeContainer}>
+      <img src={hand} alt="Hand waving" />
+      <h2 className={styles.welcomeMsg}>Welcome to Louis&apos;s shop ! </h2>
+      <div>
+        We sell all sorts of stuff, hopefully you&apos; find what you need here
+        !
+      </div>
+    </div>
+  );
 }
