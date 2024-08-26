86232c965fd2204a8d223826b7221342575dd6a4
Louis Escoffier <louis.escoffier.bagrel@gmail.com>
2024-08-26 15:00:27 +0200

    Style main navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,41 +1,49 @@
 import { NavLink } from "react-router-dom";
 import styles from "./Navbar.module.css";
 import PropTypes from "prop-types";
+import icon from "../../assets/icon.svg";
 
 export default function Navbar({ getCartLength }) {
   const cartLength = getCartLength();
   return (
     <nav className={styles.navBar}>
-      <NavLink
-        className={(navData) =>
-          navData.isActive
-            ? `${styles.active} ${styles.navLink}`
-            : styles.navLink
-        }
-        to={"/"}
-      >
-        HOME
-      </NavLink>
-      <NavLink
-        className={(navData) =>
-          navData.isActive
-            ? `${styles.active} ${styles.navLink}`
-            : styles.navLink
-        }
-        to={"/shop"}
-      >
-        SHOP
-      </NavLink>
-      <NavLink
-        className={(navData) =>
-          navData.isActive
-            ? `${styles.active} ${styles.navLink}`
-            : styles.navLink
-        }
-        to={"/cart"}
-      >
-        CART ({cartLength})
-      </NavLink>
+      <div className={styles.shopName}>
+        <img src={icon} alt="shop icon" />
+        <div>Louis&apos;s shop</div>
+      </div>
+      <div className={styles.links}>
+        <NavLink
+          className={(navData) =>
+            navData.isActive
+              ? `${styles.active} ${styles.navLink}`
+              : styles.navLink
+          }
+          to={"/"}
+        >
+          HOME
+        </NavLink>
+        <NavLink
+          className={(navData) =>
+            navData.isActive
+              ? `${styles.active} ${styles.navLink}`
+              : styles.navLink
+          }
+          to={"/shop"}
+        >
+          SHOP
+        </NavLink>
+        <NavLink
+          className={(navData) =>
+            navData.isActive
+              ? `${styles.active} ${styles.navLink} ${styles.cart}`
+              : `${styles.navLink} ${styles.cart}`
+          }
+          to={"/cart"}
+        >
+          CART
+          <div className={styles.cartLength}>{cartLength}</div>
+        </NavLink>
+      </div>
     </nav>
   );
 }
