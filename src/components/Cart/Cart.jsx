5ab6a3233c55692bec4d43cff861070929d90217
Louis Escoffier <louis.escoffier.bagrel@gmail.com>
2024-08-26 13:54:38 +0200

    Add empty cart state

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,23 +3,33 @@ import { useOutletContext } from "react-router-dom";
 import CartItem from "../CartItem/CartItem";
 
 export default function Cart() {
-  const { getCart, getCartPrice, addToCart, removeFromCart } =
+  const { getCart, getCartLength, getCartPrice, addToCart, removeFromCart } =
     useOutletContext();
   const cart = getCart();
   const cartPrice = getCartPrice().toFixed(2);
-  return (
-    <>
-      <div className="cartList">
-        {cart.map((item) => (
-          <CartItem
-            key={item.id}
-            item={item}
-            addToCart={addToCart}
-            removeFromCart={removeFromCart}
-          />
-        ))}
+
+  if (getCartLength() === 0) {
+    return (
+      <div>
+        <div>You cart is empty (for now !)</div>
+        <div>Go to the shop to add your first items to your cart !</div>
       </div>
-      <div className="cartTotal">Cart total price: {cartPrice}€</div>
-    </>
-  );
+    );
+  } else {
+    return (
+      <>
+        <div className="cartList">
+          {cart.map((item) => (
+            <CartItem
+              key={item.id}
+              item={item}
+              addToCart={addToCart}
+              removeFromCart={removeFromCart}
+            />
+          ))}
+        </div>
+        <div className="cartTotal">Cart total price: {cartPrice}€</div>
+      </>
+    );
+  }
 }
