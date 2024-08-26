8ee27ee1c957b4a5ab949b8e4d2e7caec65aa589
Louis Escoffier <louis.escoffier.bagrel@gmail.com>
2024-08-26 17:59:57 +0200

    Correct typo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,8 +7,8 @@ export default function Home() {
       <img src={hand} alt="Hand waving" />
       <h2 className={styles.welcomeMsg}>Welcome to Louis&apos;s shop ! </h2>
       <div>
-        We sell all sorts of stuff, hopefully you&apos; find what you need here
-        !
+        We sell all sorts of stuff, hopefully you&apos;ll find what you need
+        here !
       </div>
     </div>
   );
